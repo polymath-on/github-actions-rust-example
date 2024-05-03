67f3d7655a1aad9d9ae83e22c2691ef7ef656218
polymath <48149260+polymath-on@users.noreply.github.com>
2024-05-03 14:41:34 +0000

    ready for CI by JM

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/*run all functions in lib.rs */
+
+use trust::add;
+use trust::div;
+use trust::mul;
+use trust::sub;
+
+fn main() {
+    println!("add(1, 2) = {}", add(1, 2));
+    println!("sub(1, 2) = {}", sub(1, 2));
+    println!("mul(1, 2) = {}", mul(1, 2));
+    println!("div(1, 2) = {}", div(1, 2));
+}
